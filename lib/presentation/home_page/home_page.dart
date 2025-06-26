a722a59e9a2cc3b8269932960069030e68bcf75a
em07od <constantin.kurz@ing.com>
2025-06-26 14:52:21 +0200

    bloc added to home page

--- lib/presentation/home_page/home_page.dart
+++ lib/presentation/home_page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_web/application/auth/controller/authcontroller_bloc.dart';
 import 'package:flutter_web/application/matches/controller/matchescontroller_bloc.dart';
 import 'package:flutter_web/application/teams/controller/teams_controller_bloc.dart';
 import 'package:flutter_web/injections.dart';
@@ -17,30 +18,38 @@ class HomePage extends StatelessWidget {
     return MultiBlocProvider(
         providers: [
           BlocProvider(
+            create: (context) => sl<AuthControllerBloc>()..add(AuthAllEvent()),
+          ),
+          BlocProvider(
             create: (context) =>
                 sl<MatchesControllerBloc>()..add(MatchesAllEvent()),
           ),
           BlocProvider(
-            create: (context) => sl<TeamsControllerBloc>()..add(TeamsControllerAllEvent()),
+            create: (context) =>
+                sl<TeamsControllerBloc>()..add(TeamsControllerAllEvent()),
           ),
         ],
         child: Scaffold(body:
-            BlocBuilder<MatchesControllerBloc, MatchesControllerState>(
-                builder: (context, matchState) {
-          return BlocBuilder<TeamsControllerBloc, TeamsControllerState>(
-              builder: (context, teamState) {
-            if (matchState is MatchesControllerLoading ||
-                teamState is TeamsControllerLoading) {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: themeData.colorScheme.onPrimaryContainer,
-                ),
+            BlocBuilder<AuthControllerBloc, AuthControllerState>(
+                builder: (context, authState) {
+          return BlocBuilder<MatchesControllerBloc, MatchesControllerState>(
+              builder: (context, matchState) {
+            return BlocBuilder<TeamsControllerBloc, TeamsControllerState>(
+                builder: (context, teamState) {
+              if (authState is AuthControllerLoading ||
+                  matchState is MatchesControllerLoading ||
+                  teamState is TeamsControllerLoading) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: themeData.colorScheme.onPrimaryContainer,
+                  ),
+                );
+              }
+              return PageTemplate(
+                isAuthenticated: isAuthenticated,
+                child: Placeholder(),
               );
-            }
-            return PageTemplate(
-              isAuthenticated: isAuthenticated,
-              child: Placeholder(),
-            );
+            });
           });
         })));
   }

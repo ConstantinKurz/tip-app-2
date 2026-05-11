22285f900686e2804ea611dc16bcc3f9ef280ed6
em07od <constantin.kurz@ing.com>
2026-05-11 08:50:01 +0200

    klickbaren dialoge auf smartphone tablet

--- lib/presentation/core/dialogs/custom_dialog.dart
+++ lib/presentation/core/dialogs/custom_dialog.dart
@@ -26,19 +26,27 @@ class CustomDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
-    
+
     // Responsive sizing: Use provided dimensions or calculate from screen size
-    final double dialogWidth = screenWidth < 600 
-        ? screenWidth * 0.85  // Mobile: 85% of screen width
+    final double dialogWidth = screenWidth < 600
+        ? screenWidth * 0.85 // Mobile: 85% of screen width
         : width;
-    
-    final double dialogHeight = screenHeight < 700 
-        ? screenHeight * 0.70  // Mobile: 70% of screen height
+
+    final double dialogHeight = screenHeight < 700
+        ? screenHeight * 0.70 // Mobile: 70% of screen height
         : height;
 
     return Dialog(
       backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.symmetric(
+        horizontal: screenWidth < 600 ? 16 : 40,
+        vertical: screenWidth < 600 ? 24 : 40,
+      ),
       child: Container(
+        width: dialogWidth,
+        constraints: BoxConstraints(
+          maxHeight: dialogHeight,
+        ),
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.primaryContainer,
           borderRadius: BorderRadius.circular(borderRadius),
@@ -53,10 +61,8 @@ class CustomDialog extends StatelessWidget {
           children: [
             Text(dialogText, style: Theme.of(context).textTheme.headlineSmall),
             const SizedBox(height: 16),
-            SingleChildScrollView(
-              child: SizedBox(
-                width: dialogWidth,
-                height: dialogHeight,
+            Flexible(
+              child: SingleChildScrollView(
                 child: content,
               ),
             ),

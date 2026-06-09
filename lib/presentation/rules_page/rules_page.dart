e20a59f3fcdd9650bc5de8d8c5e1b14ca267f2af
em07od <constantin.kurz@ing.com>
2026-06-09 22:04:20 +0200

    pdf button for tablet

--- lib/presentation/rules_page/rules_page.dart
+++ lib/presentation/rules_page/rules_page.dart
@@ -3,6 +3,7 @@ import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_web/presentation/core/page_wrapper/page_template.dart';
+import 'package:flutter_web/constants.dart';
 
 class RulesPage extends StatefulWidget {
   static const String rulesPagePath = "/rules";
@@ -18,43 +19,67 @@ class RulesPage extends StatefulWidget {
 }
 
 class _RulesPageState extends State<RulesPage> {
+  static const String _pdfPath = 'WM2026%20onlineRegeln.pdf';
+
   final String _viewType =
       'pdf-viewer-${DateTime.now().millisecondsSinceEpoch}';
 
   @override
   void initState() {
     super.initState();
-    // Registriere den iframe als Platform View
+
     ui_web.platformViewRegistry.registerViewFactory(
       _viewType,
       (int viewId) {
         final iframe = html.IFrameElement()
-          ..src = 'WM2026%20onlineRegeln.pdf#view=FitH'
+          ..src = '$_pdfPath#view=FitH'
           ..style.border = 'none'
           ..style.width = '100%'
           ..style.height = '100%'
           ..setAttribute('scrolling', 'yes');
+
         return iframe;
       },
     );
+  }
 
-    // Auf Phones & Tablets (< 1024px) direkt PDF öffnen
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final screenWidth = MediaQuery.of(context).size.width;
-      final isMobileOrTablet = screenWidth < 1024;
-      if (isMobileOrTablet) {
-        html.window.open('WM2026%20onlineRegeln.pdf', '_blank');
-      }
-    });
+  void _openPdfInNewTab() {
+    html.window.open(_pdfPath, '_blank');
+  }
+
+  bool _isMobileOrTablet(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return screenWidth < 1024;
   }
 
   @override
   Widget build(BuildContext context) {
+    final isMobileOrTablet = _isMobileOrTablet(context);
+
     return PageTemplate(
       isAuthenticated: widget.isAuthenticated,
-      child: SizedBox.expand(
-        child: HtmlElementView(viewType: _viewType),
-      ),
+      child: isMobileOrTablet
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: ElevatedButton.icon(
+                  onPressed: _openPdfInNewTab,
+                  icon: const Icon(Icons.open_in_new),
+                  label: const Text('PDF öffnen'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black87,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 16,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : SizedBox.expand(
+              child: HtmlElementView(viewType: _viewType),
+            ),
     );
   }
 }
